4f17d62386d8551ee7a7c28fb8f5105104a6a9d6
Peter Urban <peter.urban@ugent.be>
2024-01-09 19:02:28 +0100

    fix: msvc 'i': index variable in OpenMP 'for' statement must have signed integral type

--- src/demonstrate_meson_openmp/openmp_try_parallel_pragma.hpp
+++ src/demonstrate_meson_openmp/openmp_try_parallel_pragma.hpp
@@ -15,9 +15,8 @@
 
 namespace demonstrate_meson_openmp {
 
-
 inline bool openmp_parallel_pragma_is_working_in_header() {
-  const unsigned int omp_max_threads = omp_get_max_threads();
+  const int omp_max_threads = omp_get_max_threads();
 
   // test does not make sense if there is only one thread
   if (omp_max_threads < 2) {
@@ -29,14 +28,14 @@ inline bool openmp_parallel_pragma_is_working_in_header() {
   std::vector<int> thread_responses(omp_max_threads);
 
 #pragma omp parallel for
-  for (unsigned int i = 0; i < omp_max_threads; ++i) {
+  for (int i = 0; i < omp_max_threads; ++i) {
     std::cerr << "Hello from thread " << omp_get_thread_num() << std::endl;
     thread_responses.at(omp_get_thread_num()) = 1;
     std::chrono::milliseconds duration(100);
     std::this_thread::sleep_for(duration);
   }
 
-  for (unsigned int i = 0; i < omp_max_threads; ++i) {
+  for (int i = 0; i < omp_max_threads; ++i) {
     if (thread_responses.at(i) != 1) {
       std::cerr << "thread_responses.at(" << i << ") != 1" << std::endl;
       return false;
